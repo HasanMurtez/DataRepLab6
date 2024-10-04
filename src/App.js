00e58f0639d7ca7a09a60046d1ddafc709a18b6a
Hasan Murtaza <g00419888@atu.ie>
2024-10-04 11:07:56 +0000

    Exercise 5 Complete

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Content from './components/content';
 import Header from './components/header';
 import Footer from './components/footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 function App() {
   return (
    <div className="App">
-    <h1>Hello World</h1>
-    <h2>It is {new Date().toLocaleDateString()}.</h2>
-    <Content />
-    <Footer />
-    <Header />
+
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
+      <Footer />
+    </Router>
+<Header/>
     </div>
   );
 }

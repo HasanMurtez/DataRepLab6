ac108c5fca0c3ccfae7ad2e921ee9ee9d00b2f72
Hasan Murtaza <g00419888@atu.ie>
2024-10-04 10:51:35 +0000

    Exercises 2 and 3 complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Content from './components/content';
+import Header from './components/header';
+import Footer from './components/footer';
 function App() {
   return (
    <div className="App">
     <h1>Hello World</h1>
     <h2>It is {new Date().toLocaleDateString()}.</h2>
+    <Content />
+    <Footer />
+    <Header />
     </div>
   );
 }
